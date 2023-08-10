30551c7b861a6d7be552df96aa9d48ac69224725
Kostya <forgit266@gmail.com>
2023-08-10 10:36:56 +0000

    Refactor style menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,15 @@
-import style from "../../App.module.scss";
+import style from "./menu.module.scss";
 import Box from "../../components/Box";
 import ListMenu from "../../components/ListMenu";
 
 export default function Menu(props) {
   const { data } = props;
 
-  const returnComponent = data.map((elem) => {
+  const returnComponent = data.map((elem, i) => {
     const { boxData, listMenuData, hrefSelectBox } = elem;
 
     return (
-      <div className={style.wrapper}>
+      <div key={i} className={style.wrapper}>
         <Box boxData={boxData} />
         <ListMenu listMenuData={listMenuData} hrefSelectBox={hrefSelectBox} />
       </div>
