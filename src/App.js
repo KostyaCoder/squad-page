be62d1d1f78a1a035e6718d87b7ee46d6f9ca24d
Kostya <forgit266@gmail.com>
2023-08-10 09:26:30 +0000

    Add component Menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import data from "./data.js";
 import style from "./App.module.scss";
-import Box from "./components/Box";
-import ListMenu from "./components/ListMenu/index.js";
+import Menu from "./components/Menu";
 
 function App() {
-  let returnComponent;
-  for (const elem of data) {
-    const { boxData, listMenuData, hrefSelectBox } = elem;
-
-    returnComponent = (
-      <div className={style.wrapper}>
-        <Box boxData={boxData} />
-        <ListMenu listMenuData={listMenuData} hrefSelectBox={hrefSelectBox} />
-      </div>
-    );
-  }
-
-  return returnComponent;
+  return (
+    <nav className={style.menu}>
+      <Menu data={data} />
+    </nav>
+  );
 }
 
 export default App;
